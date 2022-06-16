1c7e68e2d04f8919e172c7acd26c9d0ebbddff86
Rajendra2102 <105100871+Rajendra2102@users.noreply.github.com>
2022-06-16 11:23:42 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,14 +1,14 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://13.233.255.130:9000',
+  serverUrl: 'http://15.207.18.0:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsMithunTechnologies',
-	    'sonar.login': '219a0309ba48ec3900f263329629970429e31997',
+	    'sonar.login': 'fa7f7ca69244814f77df85f489481529532a2788',
 	    //'sonar.login': 'admin',
-	    //'sonar.password': 'admin',
+	    //'sonar.password': 'rajendra123',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
             'sonar.sourceEncoding':'UTF-8',
